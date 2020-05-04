837f326e908b30d9540e70e9f721724f7287fd68
sangeeky <sangeeky@outlook.com>
2020-05-04 16:33:42 -0500

    SOAP 5 queries

--- ColegioSOAP/src/java/ColegioSOAP/WebServiceColegio.java
+++ ColegioSOAP/src/java/ColegioSOAP/WebServiceColegio.java
@@ -87,8 +87,8 @@ public class WebServiceColegio extends Conexion {
     /**
      * Web service operation
      */
-    @WebMethod(operationName = "EstudianteCurso1")
-    public String EstudianteCurso1(@WebParam(name = "grado") String grado, @WebParam(name = "grupo") String grupo) {
+    @WebMethod(operationName = "EstudianteCurso")
+    public String EstudianteCurso(@WebParam(name = "grado") String grado, @WebParam(name = "grupo") String grupo) {
         
         String cadena="";
            
@@ -122,9 +122,44 @@ public class WebServiceColegio extends Conexion {
      * Web service operation
      */
     @WebMethod(operationName = "ConsultarMateriasMatriculadasPorEstudiante")
-    public String ConsultarMateriasMatriculadasPorEstudiante() {
-        //TODO write your implementation code here:
-        return null;
+    public String ConsultarMateriasMatriculadasPorEstudiante(@WebParam(name = "codigo") String codigo) {
+        String resultado_consulta = "";
+        
+        try{ 
+            if (db != null) {
+
+                System.out.println("la conexion fue realizada con exito");
+
+                Statement sentencia = db.createStatement();
+                ResultSet Rs = sentencia.executeQuery(
+                    "SELECT estudiantes.id_estudiante, nombres, apellidos, materias.materia, anios.anio " 
+                    +"FROM estudiantes inner join estudiante_materias "
+                    +"on estudiantes.id_estudiante = estudiante_materias.id_estudiante inner join  materias "
+                   	+"on estudiante_materias.id_materia = materias.id_materia inner JOIN anios "
+                    +"on estudiante_materias.id_anio = anios.id_anio " 
+                    
+                    +"WHERE TRIM(estudiantes.id_estudiante) = '" + codigo.trim() + "' ORDER BY anios.anio");
+                            
+                while (Rs.next()) 
+                {
+                    System.out.println( " Nombres: " + Rs.getString(2) + " Apellidos: "
+                                + Rs.getString(3) + " Materia: " + Rs.getString(4) + " Anio: " + Rs.getString(5));
+
+                    resultado_consulta += 
+                        
+                          " Codigo:" + Rs.getString(1) 
+                        + " Nombres: " + Rs.getString(2) + " Apellidos: " + Rs.getString(3) 
+                        + " Materia: " + Rs.getString(4) 
+                        + " Anio: " + Rs.getString(5);
+                }
+
+            }
+        } 
+        catch (final Exception ex) {
+            System.out.println("Error"+ex.getMessage());
+        }
+
+        return resultado_consulta ;
     }
 
     /**
@@ -132,8 +167,36 @@ public class WebServiceColegio extends Conexion {
      */
     @WebMethod(operationName = "ConsultarPromedioPorCurso")
     public String ConsultarPromedioPorCurso() {
-        //TODO write your implementation code here:
-        return null;
+        String resultado_consulta = "";
+        
+        try{ 
+            if (db != null) {
+
+                System.out.println("la conexion fue realizada con exito");
+
+                Statement sentencia = db.createStatement();
+                ResultSet Rs = sentencia.executeQuery(
+                    "SELECT anios.id_anio,cursos.id_curso,cursos.grado,cursos.grupo, AVG(nota) " 
+                    +"from cursos INNER JOIN matriculas on cursos.id_curso=matriculas.id_curso INNER JOIN anios "
+                    +"on matriculas.id_anio=anios.id_anio INNER JOIN estudiante_materias "
+                   	+"on estudiante_materias.id_anio=anios.id_anio INNER JOIN calificaciones "
+                    +"on calificaciones.id_estudiante_materia=estudiante_materias.id_estudiante_materia " 
+                    +"GROUP BY(cursos.id_curso)");
+
+                            
+                while (Rs.next()) 
+                {
+                    System.out.println( " Anio: " + Rs.getString(1) + " Id_Curso: " + Rs.getString(2) + " Grado: " + Rs.getString(3) + " Grupo: " + Rs.getString(4) + "Nota: " + Rs.getString(5));
+                    resultado_consulta += " Anio: " + Rs.getString(1) + " Id_Curso: " + Rs.getString(2) + " Grado: " + Rs.getString(3) + " Grupo: " + Rs.getString(4) + "Nota: " + Rs.getString(5);
+                }
+
+            }
+        } 
+        catch (final Exception ex) {
+            System.out.println("Error"+ex.getMessage());
+        }
+
+        return resultado_consulta ;    
     }
 
     /**
@@ -141,8 +204,39 @@ public class WebServiceColegio extends Conexion {
      */
     @WebMethod(operationName = "ConsultarPromedioPorAnio")
     public String ConsultarPromedioPorAnio() {
-        //TODO write your implementation code here:
-        return null;
+        String resultado_consulta = "";
+        
+        try{ 
+            if (db != null) {
+
+                System.out.println("la conexion fue realizada con exito");
+
+                Statement sentencia = db.createStatement();
+                ResultSet Rs = sentencia.executeQuery(
+                    "select anios.anio, avg(nota) " 
+                    +"from calificaciones inner join estudiante_materias "
+                    +"on calificaciones.id_estudiante_materia = estudiante_materias.id_estudiante_materia inner join anios "
+                    +"on estudiante_materias.id_anio = anios.id_anio "
+                    +"where anios.actual = 1");
+
+                            
+                while (Rs.next()) 
+                {
+                    System.out.println( " Anio: " + Rs.getString(1) + " Promedio Global: " + Rs.getString(2));
+
+                    resultado_consulta += 
+            
+                          " Anio:" + Rs.getString(1) 
+                        + " Promedio Global: " + Rs.getString(2) ;
+                }
+
+            }
+        } 
+        catch (final Exception ex) {
+            System.out.println("Error"+ex.getMessage());
+        }
+
+        return resultado_consulta ;
     }
 
     
