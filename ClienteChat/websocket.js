65e856f7e0b92e24963b93d5a8f7e62f4c505d87
David Santiago Pinchao <sangeeky@outlook.com>
2020-08-09 00:05:16 -0500

    Adding and Showing Users, Chat with all users

--- ClienteChat/websocket.js
+++ ClienteChat/websocket.js
@@ -1,83 +1,155 @@
 const websocket = new WebSocket("ws://localhost:8080/WebSocketJava/servidor");
-let textochat=document.getElementById("textochat");
+const username = prompt("Ingresa tu Nombre: ");
+let destinatario = "TODOS"
 
-const $chatpanel = document.getElementsByClassName("chat-box chatContainerScroll");
-const $userpanel = document.getElementsByClassName("users-container");
+let clientList = [];
+
+console.log(username);
+//websocket.send(username);
+
+let textochat = document.getElementById("textochat");
+
+const $chatpanel = document.getElementsByClassName(
+  "chat-box chatContainerScroll"
+);
+const $userpanel = document.getElementsByClassName("users");
 
 //Plantilla enviar Mensaje
-const $send_message = function (data) {
-    return`
+const $send_message = function (data, user) {
+  return `
     <li class="chat-right">
-    <div class="chat-hour">08:59 <span class="fa fa-check-circle"></span></div>
+    <div class="chat-hour"><span class="fa fa-check-circle"></span></div>
     <div class="chat-text">${data}</div>
     <div class="chat-avatar">
         <img src="https://www.bootdey.com/img/Content/avatar/avatar5.png" alt="Retail Admin">
-        <div class="chat-name">${data}</div>
+        <div class="chat-name">${user}</div>
     </div>
     </li>
-    `;   
+    `;
 };
 
 //Plantilla recibir mensaje
-const $recive_message = function (data) {
-    return`
+const $recive_message = function (data, user) {
+  return `
     <li class="chat-left">
       <div class="chat-avatar">
           <img src="https://www.bootdey.com/img/Content/avatar/avatar3.png" alt="Retail Admin">
-          <div class="chat-name">${data}</div>
+          <div class="chat-name">${user}</div>
       </div>
       <div class="chat-text">${data}</div>
-      <div class="chat-hour">08:57 <span class="fa fa-check-circle"></span></div>
+      <div class="chat-hour"><span class="fa fa-check-circle"></span></div>
   </li>`;
 };
-
-const $user_list = `
+//Plantilla Lista de Usuarios
+const $user_template = (data) => {
+  return `
 <li class="person" data-chat="person1">
 <div class="user">
     <img src="https://www.bootdey.com/img/Content/avatar/avatar3.png" alt="Retail Admin">
     <span class="status busy"></span>
 </div>
 <p class="name-time">
-    <span class="name">${"username"}</span>
+    <span class="name">${data}</span>
 </p>
 </li>
-`
-
-
+`;
+};
 
 //Creamos un nuevo elemetno que se va a agregar al HTML
 function createTemplate(HTMLstring) {
-    const html = document.implementation.createHTMLDocument();
-    html.body.innerHTML = HTMLstring;
-    return html.body.children[0];
+  const html = document.implementation.createHTMLDocument();
+  html.body.innerHTML = HTMLstring;
+  return html.body.children[0];
 }
 
-
-
-
-  websocket.onopen = function (event) {
-    if (event.data === undefined) return;
-    escribirRespuesta(event.data);
-  };
-  websocket.onmessage = function (event) {
-    escribirRespuesta(event.data);
-  };
-  websocket.onclose = function () {
-    escribirRespuesta("Conexion Cerrada");
-  };
-
-function sendMessage() {
-  let texto = document.getElementById("txtmensaje").value;
-  websocket.send(texto);
+function addEventClick($element) {
+  $element.addEventListener("click", () => {
+    alert('click!')
+    //showModal($element);
+  });
 }
+
+//Reciibir Mensajees
+websocket.onopen = function (event) {
+  if (event.data === undefined ) return;
+  escribirRespuesta(event.data);
+};
+
+// Al recibir un mensaje
+websocket.onmessage = function (event) {
+  let array = event.data.split("#");
+  let operacion = array[0];
+  array.shift();
+  array.pop();
+  switch (operacion) {
+    case "Conexion Creada":
+      websocket.send(username);
+      setInterval(() => {
+        //console.log("Entramos");
+        websocket.send("pls");
+      }, 1000)
+      setInterval(() => {
+        $userpanel.item(0).innerHTML=''
+        clientList.forEach((element) => {
+          const userElement = createTemplate($user_template(element))
+          $userpanel.item(0).append(userElement)
+          // Click
+          addEventClick(userElement);
+        })
+      }, 1000)
+      break;
+    
+    case "ListaUsuarios":
+      clientList = [];
+      array.forEach(element => {
+        clientList.push(element);
+      });
+      break;
+  
+    default:
+      escribirRespuesta(event.data);
+      break;
+  }
+};
+
 function closeSocket() {
   websocket.close();
 }
-function escribirRespuesta(texto) {
-    
-    $chatpanel.item(0).append(createTemplate($recive_message(texto)))
-    //$chatpanel.item(0).append(createTemplate($send_message(texto)))
 
-    //textochat.innerHTML+="</br>"+texto;
+
+//Al Cerrar Conexion
+websocket.onclose = function () {
+  escribirRespuesta("Conexion Cerrada");
+};
+
+function onKeyUp(event) {
+  let keycode = event.keyCode;
+  if(keycode == '13'){
+    sendMessage();
+    document.getElementById("txtmensaje").value = "";
+  }
 }
+
+function sendMessage() {
+  
+  let texto = document.getElementById("txtmensaje").value;
+  websocket.send(texto+"#"+destinatario+"#"+username);
+  document.getElementById("txtmensaje").value = "";
+}
+
+function escribirRespuesta(texto) {
+  let array = event.data.split("#");
+  let mensaje = array[0];
+  let usuario = array[1];
+
+  if(usuario == username)
+  {
+    $chatpanel.item(0).append(createTemplate($send_message(mensaje, usuario)));
+  }
+  else{
+    $chatpanel.item(0).append(createTemplate($recive_message(mensaje, usuario)));
+  }
+
+}
+
 
--- WebSocketJava/src/java/com/sangeeky/ServerWebSocket.java
+++ WebSocketJava/src/java/com/sangeeky/ServerWebSocket.java
@@ -4,7 +4,15 @@
  * and open the template in the editor.
  */
 package com.sangeeky;
+
 import java.io.IOException;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.OnOpen;
 import javax.websocket.OnMessage;
 import javax.websocket.OnError;
@@ -14,37 +22,100 @@ import javax.websocket.server.ServerEndpoint;
 
 /**
  *
- * @author vauxr
+ * @author sangeeky
  */
 @ServerEndpoint("/servidor")
 public class ServerWebSocket {
-    
-    @OnOpen
-    public void onOpen(Session session){
-        //Nuevo Ciente se Conecta
-        System.out.println("Solicitud conexion "+session.getId());
-        try{
-            session.getBasicRemote().sendText("Conexion Creada"); // Respuesta al Cliente que se conecto
-        }catch(IOException ex){
-            System.out.println("Error "+ex.getMessage());
-        }
-    }
-    
-    @OnMessage
-    public void onMessage(String message,Session session){
-        try{
-            for(Session s: session.getOpenSessions()){
-                 s.getBasicRemote().sendText(message);
-                 System.out.println("OK"+message); /// Aqui mandamos a todos los usuarios
+
+    static Vector<Client> clientList = new Vector<>();
+
+    Timer timer = new Timer();
+
+    public int getIndexClient(Session session) {
+        int indexClient = 0;
+        for (Client c : clientList) {
+            if (c.clientSession.equals(session)) {
+                break;
             }
-            
-        }catch(IOException ex){
-            System.out.println("Error "+ex.getMessage());
+            indexClient++;
+        }
+        return indexClient;
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        //Nuevo Ciente se Conecta
+        System.out.println("Solicitud conexion " + session.getId());
+        try {
+            Client cliente = new Client(session, ""); // Creamos el objeto Cliente
+            clientList.add(cliente); //Agregamos a nuestro array de CLientes
+            session.getBasicRemote().sendText("Conexion Creada#"); // Respuesta al Cliente que se conecto
+        } catch (IOException ex) {
+            System.out.println("Error " + ex.getMessage());
         }
     }
-    @OnClose
-    public void onClose(Session session){
-        System.out.println("Session Terminada:"+session.getId()); //Cerramos el WebSocket
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        Client cliente;
+        try {
+            int indexClient = getIndexClient(session);
+            cliente = clientList.get(indexClient);
+
+            //Recibimos el Nombre de usuario
+            if (cliente.username.equals("")) {
+                clientList.get(indexClient).username = message;
+                cliente = clientList.get(indexClient);
+                System.out.println("Solicitud conexion " + cliente.username);
+            } else if (message.equals("pls")) //Devolvemos los Usuarios Conectados
+            {
+                if (!clientList.isEmpty()) {
+                    String listaUsuarios = "ListaUsuarios#";
+                    //clientList.forEach((c) -> listaUsuarios.concat(c.username + "#"));
+                    for (Client c : clientList) {
+                        listaUsuarios += c.username + "#";
+                    }
+                    for (Session s : session.getOpenSessions()) {
+                        try {
+                            s.getBasicRemote().sendText(listaUsuarios);
+                        } catch (IOException ex) {
+                            Logger.getLogger(ServerWebSocket.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            } else {   //Broadcast a todos los usuarios
+                StringTokenizer st = new StringTokenizer(message, "#");
+                String mensaje = st.nextToken();
+                String destinatario = st.nextToken();
+                String usuario = st.nextToken();
+                
+                for (Session s : session.getOpenSessions()) {
+                    s.getBasicRemote().sendText(mensaje.concat("#"+usuario));
+                    System.out.println("OK" + message); /// Aqui mandamos a todos los usuarios
+                }
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error " + ex.getMessage());
+        }
     }
-         
+
+    @OnClose
+    public void onClose(Session session) {
+        clientList.removeElementAt(getIndexClient(session));
+        System.out.println("Session Terminada:" + session.getId()); //Cerramos el WebSocket
+    }
+
+}
+
+class Client {
+
+    Session clientSession;
+    String username;
+
+    public Client(Session session, String username) {
+        this.clientSession = session;
+        this.username = username;
+    }
+
 }
